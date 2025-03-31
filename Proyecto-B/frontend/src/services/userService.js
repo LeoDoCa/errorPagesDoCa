c6bed379f03c6086f4bfd1a3079cb1ac2c7f28cd
LeoDoCa <20223tn049@utez.edu.mx>
2025-03-31 14:25:30 -0600

    Agregue lo de la tarea UIII T2 pt.2

--- Proyecto-B/frontend/src/services/userService.js
+++ Proyecto-B/frontend/src/services/userService.js
@@ -1,7 +1,6 @@
 import apiClient from "./tokenService";
 
 const userService = {
-  // Obtener todos los usuarios
   getAllUsers: async () => {
     try {
       const response = await apiClient.get("api/");
@@ -11,7 +10,6 @@ const userService = {
     }
   },
 
-  // Obtener un usuario por ID
   getUserById: async (id) => {
     try {
       const response = await apiClient.get(`api/${id}/`);
@@ -21,7 +19,6 @@ const userService = {
     }
   },
 
-  // Actualizar un usuario
   updateUser: async (id, userData) => {
     try {
       const response = await apiClient.put(`api/${id}/`, userData);
@@ -31,7 +28,6 @@ const userService = {
     }
   },
 
-  // Eliminar un usuario
   deleteUser: async (id) => {
     try {
       const response = await apiClient.delete(`api/${id}/`);
@@ -41,18 +37,13 @@ const userService = {
     }
   },
 
-  // Obtener el ID del usuario actualmente logueado
   getCurrentUserId: () => {
     const token = localStorage.getItem("accessToken");
     if (!token) return null;
     
-    // Decodificar el token JWT para obtener la información del usuario
     try {
-      // JWT está dividido en tres partes separadas por puntos
       const payload = token.split(".")[1];
-      // Decodificar la parte del payload
       const decodedPayload = JSON.parse(atob(payload));
-      // Obtener el ID del usuario desde el payload
       return decodedPayload.user_id;
     } catch (error) {
       console.error("Error decoding token:", error);
